7d786bd408f0d3f1b6c67de6641a22ed24e017d2
dellod <daryl.dang4@gmail.com>
2022-10-30 19:42:41 -0600

    Add increasing snake length and reset ability

--- games/snake.py
+++ games/snake.py
@@ -5,7 +5,8 @@
 
 ####################################################################################################
 # Snake
-# TODO: insert snake description
+# Classic snake game where the objective is to get as many points as possible. Leaving the screen or
+# consuming any part of the snake's tail will result in a loss and the player will have to restart.
 ####################################################################################################
 
 # IMPORTS
@@ -37,6 +38,15 @@ X_UPDATE        = 0
 Y_UPDATE        = 0
 
 ### FUNCTIONS
+def reset_position() -> None:
+    """Resets the position of the snake and makes it so that it doesn't move."""
+    global X_POS, Y_POS, X_UPDATE, Y_UPDATE
+    X_POS = SCREEN_WIDTH / 2
+    Y_POS = SCREEN_HEIGHT / 2
+    X_UPDATE = 0
+    Y_UPDATE = 0
+
+
 def create_message(message: str,
                    colour: tuple,
                    x_pos: float,
@@ -49,6 +59,7 @@ def create_message(message: str,
 
 
 def generate_random_food_position() -> tuple:
+    """Generates random food position on the screen."""
     food_pos = \
         (round(random.randrange(0, SCREEN_WIDTH - SNAKE_SIZE[0]) / SNAKE_SIZE[0]) * SNAKE_SIZE[0],
          round(random.randrange(0, SCREEN_HEIGHT - SNAKE_SIZE[1]) / SNAKE_SIZE[1]) * SNAKE_SIZE[1])
@@ -56,6 +67,7 @@ def generate_random_food_position() -> tuple:
 
 
 def check_grid_collision(snake_pos: tuple, food_pos: tuple):
+    """Check if the snake has made collision with the position of a food."""
     if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
         return True
     else:
@@ -79,6 +91,10 @@ def main() -> None:
 
     # Set up font style
     font_style = pygame.font.SysFont(None, 50)
+
+    # Snake information
+    snake_pos_list = []
+    snake_length = 1
 
     # Food variables
     food_pos = generate_random_food_position()
@@ -115,6 +131,13 @@ def main() -> None:
                     # Move snake to the down
                     X_UPDATE = NO_MOVEMENT
                     Y_UPDATE = MOVEMENT_SIZE
+                if did_player_lose and event.key == pygame.K_c:
+                    # Reset the game
+                    reset_position()
+                    snake_pos_list = []
+                    snake_length = 1
+                    did_player_lose = False
+                    score = 0
 
         # 2. Check conditions
         if X_POS > SCREEN_WIDTH or X_POS < 0 or Y_POS > SCREEN_HEIGHT or Y_POS < 0:
@@ -129,23 +152,41 @@ def main() -> None:
         create_message(score_message, WHITE, 10, 10, display, font_style)
 
         # Draw snake
-        pygame.draw.rect(display, GREEN, [X_POS, Y_POS, SNAKE_SIZE[0], SNAKE_SIZE[1]])
+        for snake_block in snake_pos_list:
+            pygame.draw.rect(display, GREEN, [snake_block[0], snake_block[1], SNAKE_SIZE[0], SNAKE_SIZE[1]])
 
         # Draw food
         pygame.draw.rect(display, RED, [food_pos[0], food_pos[1], SNAKE_SIZE[0], SNAKE_SIZE[1]])
 
         # Check if snake is on top of food
         if check_grid_collision((X_POS, Y_POS), food_pos):
-            score += 1
-            food_pos = generate_random_food_position()
+            score += 1 # Get a point
+            snake_length += 1 # Increase length
+            food_pos = generate_random_food_position() # Generate new food position
 
         # Increase length of snake
-        # TODO
+        curr_snake_head = [X_POS, Y_POS]
+        snake_pos_list.append(curr_snake_head)
+        if len(snake_pos_list) > snake_length:
+            del snake_pos_list[0]
+
+        # Check if snake has eaten its own body
+        for snake_block in snake_pos_list[:-1]:
+            if snake_block == curr_snake_head:
+                did_player_lose = True
 
         # Check if player lost
         if did_player_lose:
-            create_message("You lost!", RED, SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/2, display, font_style)
-            # TODO: They have to press a key here to restart the game
+            create_message("You lost!",
+                           RED,
+                           SCREEN_WIDTH/2 - 80,
+                           SCREEN_HEIGHT/2,
+                           display, font_style)
+            create_message("Press 'C' if you would like to play again.",
+                           RED,
+                           SCREEN_WIDTH/8,
+                           SCREEN_HEIGHT/2 + 40,
+                           display, font_style)
         else:
             X_POS += X_UPDATE
             Y_POS += Y_UPDATE
